38595d4a40354ead67b80d167fbcdfc30d80434a
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-07-30 10:11:28 +0530

    Update index.js

--- auth_app/src/contexts/index.js
+++ auth_app/src/contexts/index.js
@@ -1,2 +1,27 @@
-import { createContext } from "react";
-export const AuthenticatedUserContext = createContext({});
+import React, { createContext, useState, useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../config/firebase';
+
+export const AuthenticatedUserContext = createContext();
+
+export const AuthenticatedUserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthenticatedUserContext.Provider value={{ user }}>
+      {children}
+    </AuthenticatedUserContext.Provider>
+  );
+};
