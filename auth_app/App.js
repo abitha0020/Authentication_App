71a3899760448655da1ec297a879c47de7ed1cea
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-07-30 10:46:11 +0530

    Update App.js

--- auth_app/App.js
+++ auth_app/App.js
@@ -1,99 +1,11 @@
-import 'react-native-gesture-handler';
-import React, { useState, useContext, useEffect } from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { ActivityIndicator, SafeAreaView } from "react-native";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./src/config/firebase";
-import { createStackNavigator } from "@react-navigation/stack";
-import { AuthenticatedUserContext } from "./src/contexts";
-import { StatusBar } from "react-native";
-import Signup from './src/screens/signup';
-import Login from "./src/screens/login";
-import Home from './src/screens/home';
-import Profile from './src/screens/profile';
-import { Platform } from 'react-native';
-
-const AuthenticatedUserProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-  return (
-    <AuthenticatedUserContext.Provider value={{ user, setUser }}>
-      {children}
-    </AuthenticatedUserContext.Provider>
-  );
-};
+import React, { useState } from "react";
+import { AuthenticatedUserProvider } from "./src/contexts/index";
+import RootNavigator from "./src/navigators/navigator";
 
 export default function App() {
   return (
     <AuthenticatedUserProvider>
-      <RootComponent />
+      <RootNavigator />
     </AuthenticatedUserProvider>
   );
 }
-
-const RootComponent = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const { setUser } = useContext(AuthenticatedUserContext);
-
-  useEffect(() => {
-    const unsubscribeAuth = onAuthStateChanged(
-      auth,
-      async (authenticatedUser) => {
-        if (authenticatedUser) {
-          await authenticatedUser.reload();
-          setUser(authenticatedUser);
-        } else {
-          setUser(null);
-        }
-        setIsLoading(false);
-      }
-    );
-    return unsubscribeAuth; 
-  }, [setUser]);
-
-  if (isLoading) {
-    return (
-      <SafeAreaView
-        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-      >
-        <ActivityIndicator size="large" />
-      </SafeAreaView>
-    );
-  }
-
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <NavigationContainer>
-        <MainNavigator />
-      </NavigationContainer>
-    </>
-  );
-};
-
-const MainNavigator = () => {
-  const { user } = useContext(AuthenticatedUserContext);
-  return user ? <AppStack /> : <AuthStack />;
-};
-
-const Stack = createStackNavigator();
-
-const AppStack = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Profile" component={Profile} />
-    </Stack.Navigator>
-  );
-};
-
-
-
-const AuthStack = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Signup" component={Signup} />
-      <Stack.Screen name="Home"   component={Home} />
-    </Stack.Navigator>
-  );
-};
